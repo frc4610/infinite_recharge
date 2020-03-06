29088ccdd617b307e2d0dcbfabed7ac44ffe3a5e
Dea-Sea <35466700+Dea-Sea@users.noreply.github.com>
2020-03-05 18:20:11 -0600

    basic things for limit switch

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -14,16 +14,19 @@ import com.revrobotics.ControlType;
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Climber extends SubsystemBase {
   private CANSparkMax climber;
   private CANPIDController climbPID;
   private CANEncoder climbEncoder;
+  private DigitalInput limitSwitch;
   /**
    * Creates a new Climber.
    */
   public Climber() {
+    limitSwitch = new DigitalInput(0);
     climber = new CANSparkMax(5, MotorType.kBrushless);
     climber.setIdleMode(IdleMode.kBrake);
     climber.burnFlash();
@@ -38,7 +41,13 @@ public class Climber extends SubsystemBase {
 
   public void set(double speed)
   {
+    if(!limitState() && speed > 0)
     climber.set(speed);
+  }
+
+  public boolean limitState()
+  {
+    return limitSwitch.get();
   }
 
   public double getEnc()
