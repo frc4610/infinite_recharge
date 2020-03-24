136a12a90a0482a9205a132caafd5a08957742d2
Jacob Wells <jpwells0105@hotmail.com>
2020-03-24 15:15:50 -0500

    Starting test the concept of PathWeaver, started

--- src/main/java/frc/robot/commands/leftencoderMovement.java
+++ src/main/java/frc/robot/commands/leftencoderMovement.java
@@ -10,7 +10,6 @@ package frc.robot.commands;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.RobotContainer;
 import frc.robot.subsystems.DriveBase;
 import frc.robot.subsystems.encoder;
 import frc.robot.subsystems.navX;
